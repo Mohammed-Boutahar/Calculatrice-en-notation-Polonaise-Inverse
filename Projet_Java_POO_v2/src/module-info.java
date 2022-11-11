e3f5f14646cefe245a4c3e10be0dac9904aadde8
Boutahar Mohammed <98554453+Mohammed-Boutahar@users.noreply.github.com>
2022-11-11 15:28:01 +0100

    Delete Projet_Java_POO_v2 directory

--- Projet_Java_POO_v2/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Projet_Java_POO_v2 {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires java.desktop;
-	requires javafx.base;
-	
-	opens vue to javafx.graphics, javafx.fxml;
-}
